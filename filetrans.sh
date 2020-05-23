1e1b583944214738b1e4dc1a04917f555163cccb
Kenzie Coddens <kenzie.coddens.y1678@student.hogent.be>
2020-05-23 16:33:56 +0200

    Update filetrans.sh

--- filetrans.sh
+++ filetrans.sh
@@ -2,7 +2,7 @@
 apt-get update
 apt-get -y upgrade
 apt-get -y install sshpass
-sshpass -p 'haai' sftp -o StrictHostKeyChecking=accept-new -P 5151 -oBatchMode=no -b - TestU@213.119.34.127 << !
+sshpass -p 'haai' sftp -o StrictHostKeyChecking=accept-new -P 5151 -oBatchMode=no -b - TestU@server << !
    cd documents
    put /workspace/MessageUtil/bin/Release/netcoreapp3.1/win10-x64/messageutil-win10-x64.tar.gz
    bye
