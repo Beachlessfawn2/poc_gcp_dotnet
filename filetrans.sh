a55bf1436416cf569579375694038105306e11c6
Beachlessfawn2 <kenzie.coddens.y1678@student.hogent.be>
2020-05-23 15:32:13 +0200

    [Code] Testing CD

--- filetrans.sh
+++ filetrans.sh
@@ -2,8 +2,7 @@
 apt-get update
 apt-get -y upgrade
 apt-get -y install sshpass
-export SSHPASS='temp'
-sshpass -e sftp -P 5151 -oBatchMode=no -b - KenzieC@213.119.34.127 << !
+sshpass -p 'temp' -e sftp -P 5151 -oBatchMode=no -b - KenzieC@213.119.34.127 << !
    put /workspace/MessageUtil/bin/Release/netcoreapp3.1/win10-x64/messageutil-win10-x64.tar.gz
    bye
 !
